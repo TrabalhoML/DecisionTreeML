b395afbcc4a6cae93372a98cbb64cd2f4ed85cd0
carlossdparaujo <carlossdparaujo@gmail.com>
2015-05-30 19:34:45 -0300

    Criada funçao para fazer o aprendizado recursivamente, arvore de teste pode ser vista no arquivo teste.txt

--- decisionnode.py
+++ decisionnode.py
@@ -1,5 +1,19 @@
 class decisionnode:
-	def __init__(self, attr=-1, value_node_dic=None, value_result_dic=None):
+	def __init__(self, attr=-1, value_node_dic=None, result=None):
 		self.attr = attr
 		self.value_node_dic = value_node_dic
-		self.value_result_dic = value_result_dic
+		self.result = result
+
+	def __str__(self):
+		if self.value_node_dic != None:
+			dic_str = "{"
+			counter = 0
+			for key in self.value_node_dic:
+				dic_str += key + " : " + str(self.value_node_dic[key]) 
+				if counter != len(self.value_node_dic) - 1:
+					dic_str += ", "
+					counter += 1
+			dic_str += "}"
+		else:
+			dic_str = "None"
+		return ("Decision Node: [attr: " + str(self.attr) + ", value_to_node: " + dic_str + ", result: " + str(self.result) + "]")
--- tree_training.py
+++ tree_training.py
@@ -1,16 +1,35 @@
 import math
+from decisionnode import decisionnode
 
 def tree_training(rows):
-	tree = None
+	result = None
+	value_to_node = None
+	attr = get_best_attr(rows)
+	if attr == None:
+		result = rows[0][len(rows[0]) - 1]
+	else:
+		value_to_node = {}
+		node_values = divide_rows_for_attr_values(rows, attr)
+		for key in node_values:
+			value_to_node[key] = tree_training(node_values[key])
+
+	node = decisionnode(attr, value_to_node, result)
+	return node
+
+def get_best_attr(rows):
 	most_valuable_attr = -1
 	max_information_gain = -1
 	for i in range(0, len(rows[0]) - 1):
 		attr_gain = information_gain(rows, i)
-		print("Attribute of index " + str(i) + " has information gain of " + str(attr_gain))
+		#print("Attribute of index " + str(i) + " has information gain of " + str(attr_gain))
 		if attr_gain > max_information_gain:
 			max_information_gain = attr_gain
 			most_valuable_attr = i
-	print("The root attribute is " + str(most_valuable_attr))
+	#print("The root attribute is " + str(most_valuable_attr))
+	if max_information_gain == 0:
+		return None
+	else:
+		return most_valuable_attr
 
 def information_gain(rows, i):
 	rows_entropy = entropy(rows)
@@ -54,4 +73,4 @@ my_data=[['slashdot','USA','yes','<20','None'],
 ['google','UK','yes','<20','Basic'],
 ['kiwitobes','France','yes','<20','Basic']]
 
-tree_training(my_data)
+print(tree_training(my_data))
