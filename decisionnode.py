aa94daa8afb935731696b77660fe05122f7f2d73
carlossdparaujo <carlossdparaujo@gmail.com>
2015-05-30 17:04:17 -0300

    Classe de decision node criada.

---
+++ decisionnode.py
@@ -0,0 +1,5 @@
+class decisionnode:
+	def __init__(self, attr=-1, value_node_dic=None, value_result_dic=None):
+		self.attr = attr
+		self.value_node_dic = value_node_dic
+		self.value_result_dic = value_result_dic
