16caec6a5e4a1bfdb63eb2096fa911b5e33eedcd
carlossdparaujo <carlossdparaujo@gmail.com>
2015-06-07 19:10:58 -0300

    Nomeando atributos e trocando input para uma lista de dicionários, para evitar o problema de valueerror na classificaçao das arvores dividas por atributos.

--- main.py
+++ main.py
@@ -2,30 +2,30 @@ from bayes import calculateBayes
 from random_forest import split_dataset
 from decisiontree import decisiontree
 
-my_data=[['slashdot','USA','yes','<20','None'],
-['google','France','yes','>20','Premium'],
-['digg','USA','yes','>20','Basic'],
-['kiwitobes','France','yes','>20','Basic'],
-['google','UK','no','>20','Premium'],
-['(direct)','New Zealand','no','<20','None'],
-['(direct)','UK','no','>20','Basic'],
-['google','USA','no','>20','Premium'],
-['slashdot','France','yes','<20','None'],
-['digg','USA','no','<20','None'],
-['google','UK','no','<20','None'],
-['kiwitobes','UK','no','<20','None'],
-['digg','New Zealand','yes','<20','Basic'],
-['slashdot','UK','no','>20','None'],
-['google','UK','yes','<20','Basic'],
-['kiwitobes','France','yes','<20','Basic']]
+my_data=[{0 : 'slashdot', 1 : 'USA', 2 : 'yes', 3 : '<20', 'Result' : 'None'},
+{0 : 'google', 1 : 'France', 2 : 'yes', 3 : '>20', 'Result' : 'Premium'},
+{0 : 'digg', 1 : 'USA', 2 : 'yes', 3 : '>20', 'Result' : 'Basic'},
+{0 : 'kiwitobes', 1 : 'France', 2 : 'yes', 3 : '>20', 'Result' : 'Basic'},
+{0 : 'google', 1 : 'UK', 2 : 'no', 3 : '>20', 'Result' : 'Premium'},
+{0 : '(direct)', 1 : 'New Zealand', 2 : 'no', 3 : '<20', 'Result' : 'None'},
+{0 : '(direct)', 1 : 'UK', 2 : 'no', 3 : '>20', 'Result' : 'Basic'},
+{0 : 'google', 1 : 'USA', 2 : 'no', 3 : '>20', 'Result' : 'Premium'},
+{0 : 'slashdot', 1 : 'France', 2 : 'yes', 3 : '<20', 'Result' : 'None'},
+{0 : 'digg', 1 : 'USA', 2 : 'no', 3 : '<20', 'Result' : 'None'},
+{0 : 'google', 1 : 'UK', 2 : 'no', 3 : '<20', 'Result' : 'None'},
+{0 : 'kiwitobes', 1 : 'UK', 2 : 'no', 3 : '<20', 'Result' : 'None'},
+{0 : 'digg', 1 : 'New Zealand', 2 : 'yes', 3 : '<20', 'Result' : 'Basic'},
+{0 : 'slashdot', 1 : 'UK', 2 : 'no', 3 : '>20', 'Result' : 'None'},
+{0 : 'google', 1 : 'UK', 2 : 'yes', 3 : '<20', 'Result' : 'Basic'},
+{0 : 'kiwitobes', 1 : 'France', 2 : 'yes', 3 : '<20', 'Result' : 'Basic'}]
 
-case = ['kiwitobes','New Zealand','yes','>20']
+case = {0 : 'kiwitobes', 1 : 'New Zealand', 2 : 'yes', 3 : '>20'}
 
-tree = decisiontree(training_data=my_data)
+#tree = decisiontree(training_data=my_data)
 forest = split_dataset(my_data)
-print(str(tree))
-print(tree.classify(case)) #KeyError
-print(calculateBayes(my_data, case))
+#print(str(tree))
+#print(tree.classify(case)) #KeyError
+#print(calculateBayes(my_data, case))
 print(forest[0])
 print(forest[0].classify(case))
 print(forest[1])
